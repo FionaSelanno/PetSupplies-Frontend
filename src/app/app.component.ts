ed65bcfa8bc596caec3929b6d84e9af0d238af0b
FionaSelanno <fcs_fiona@hotmail.com>
2017-05-05 18:02:24 +0200

    turn data into Javascript object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService} from './product.service';
+import { Response } from '@angular/http'
 
 
 @Component({
@@ -12,7 +13,10 @@ export class AppComponent {
   constructor(private productService: ProductService) {}//to inject the ProductService component
   onGet(){
    return this.productService.getProducts().subscribe(
-     (response) => console.log(response),
+     (response: Response) => { // import Response from @angular/http
+       const data = response.json(); //looks in the body property, gets the data from there and turns it into a Javascript object
+       console.log(data);
+     },
      (error) => console.log(error)
    );
   };
