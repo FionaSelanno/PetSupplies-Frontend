70accf4b46c31460718a3c02439d2217cc8d7b05
FionaSelanno <fcs_fiona@hotmail.com>
2017-05-08 15:48:38 +0200

    header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styles:[`
+    .page-header {
+        color: #5250A3;
+        }
+    `]
+})
+
+export class HeaderComponent
+{}
