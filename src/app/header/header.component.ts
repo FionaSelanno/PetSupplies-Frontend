a04de0c64e5878a859e8eec73c6974138a321ed5
FionaSelanno <fcs_fiona@hotmail.com>
2017-05-10 11:16:12 +0200

    added shoppingcard icon to nav-bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,11 +3,7 @@ import { Component} from '@angular/core';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styles:[`
-    .page-header {
-        color: #5250A3;
-        }
-    `]
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent
