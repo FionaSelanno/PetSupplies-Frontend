09ae4143a80276e53a4debeaa8edd2edaaf87527
FionaSelanno <fcs_fiona@hotmail.com>
2017-05-17 11:49:46 +0200

    display total added products in HeaderComponent
    By subscribing to the Observable it's able to listen to changes in
    another component. When clicking on a product in a ProducListComponent,
    the HeaderComponent is able to listen to it. It displays total added
    products.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CustomerService} from 'app/customer.service';
 import { Subscription} from 'rxjs/subscription'
 
@@ -8,17 +8,21 @@ import { Subscription} from 'rxjs/subscription'
     styleUrls: ['./header.component.css'],
 })
 
-export class HeaderComponent{   
-    message: any;
+export class HeaderComponent implements OnInit{   
+    //message: any;
     subscription: Subscription;
     nrOfProducts = this.cs.productArray.length;
-
+    
 
     constructor(private cs:CustomerService ){
     //subscribed aan de observable als die veranderd wordt dit geupdate
     this.subscription = this.cs.getMessage()
-    .subscribe(message => { 
-        this.nrOfProducts = message.totaal
-        console.log(this.message) });  
+    .subscribe(response => { 
+        this.nrOfProducts = response.products.length
+        console.log(this.nrOfProducts) });  
     }
+
+    ngOnInit() {
+    this.cs.sessionAddProducts();
+   };
 }
